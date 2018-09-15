117ccbfa399f1d0d0fa7640bedcdcb32599015d5
esolar07 <esolar07@gmail.com>
2018-09-15 18:59:43 -0400

    added Link module to navigation for navigating routes

---
+++ src/containers/Parents.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+class Parents extends Component {
+
+    render(){
+        return (
+            <div>
+                <h1>Parents </h1>
+                <h1>Parents </h1>
+
+                <h1>Parents </h1>
+
+                <h1>Parents </h1>
+
+            </div>
+        )
+    }
+}
+
+export default Parents;
