4a927bd1c2b5356b4789f4cce67c76a0f6059e12
Juan Flores <jflifestyles@gmail.com>
2018-09-15 17:12:56 -0400

    added header and footer components

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 
 class Home extends Component {
-    
+
     render(){
         return (
             <div>
                 <h1>Home </h1>
+                <Button variant="contained" color="primary">
+                  Lets get this started
+                </Button>
             </div>
         )
     }
